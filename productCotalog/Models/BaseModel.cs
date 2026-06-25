495df17f5fcc7d4550b4d80080b327f6367d57af
Daniyar Kashkaraliev <daniyar.kashkaraliev@gmail.com>
2026-06-25 14:49:01 +0600

    baseModels (sortModel, paginationModel), this models we using in different entities

---
+++ productCotalog/Models/BaseModel.cs
@@ -0,0 +1,13 @@
+﻿namespace productCotalog.Models
+{
+    public enum SortModelEnum
+    { 
+        NONE,
+        ASC, 
+        DESC
+    }
+    public class SortModel{
+        public SortModelEnum Value;
+        public string? DisplayName;
+    }
+}
